03ab0d3b40f75a6414897a7d61c937b1055c8d37
Kayla Lee <kel7@sfu.ca>
2022-10-27 16:29:01 -0700

    Initial Commit

---
+++ app/src/main/java/ca/cmpt276/chromiumproject/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.cmpt276.chromiumproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
