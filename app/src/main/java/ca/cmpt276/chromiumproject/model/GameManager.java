0effd0f4adc6eef57c04a57755713f44a78ba41d
cza95 <chelsea_zhao@sfu.ca>
2022-10-29 15:20:13 -0700

    [Model] implement GameManager and GamePlayed classes and fields
    add iterator to GameManager class and function to add new game config.

--- app/src/main/java/ca/cmpt276/chromiumproject/model/GameManager.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/model/GameManager.java
@@ -1,9 +1,12 @@
 package ca.cmpt276.chromiumproject.model;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
-public class GameManager {
+public class GameManager implements Iterable<GameConfig>{
     List<GameConfig> gameConfigs = new ArrayList<>();
 
     private static GameManager instance;
@@ -18,4 +21,21 @@ public class GameManager {
         return instance;
     }
 
+    public void addNewGameConfig(GameConfig config) {
+        gameConfigs.add(config);
+    }
+
+    public int getNumGameConfigs() {
+        return gameConfigs.size();
+    }
+
+    public GameConfig getGameConfigByIndex(int index) {
+        return gameConfigs.get(index);
+    }
+
+    @NonNull
+    @Override
+    public Iterator<GameConfig> iterator() {
+        return gameConfigs.iterator();
+    }
 }
--- app/src/main/java/ca/cmpt276/chromiumproject/model/GamePlayed.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/model/GamePlayed.java
@@ -1,5 +1,43 @@
 package ca.cmpt276.chromiumproject.model;
 
-public class GamePlayed {
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class GamePlayed {
+    private static final int MIN_PLAYERS = 1;
+    private int numPlayers;
+    private int combinedScore;
+    // TODO: implement actual achievement stuff once it's ready
+    private final String achievement;
+
+    private LocalDateTime creationTime;
+    private static final DateTimeFormatter DT_FORMAT = DateTimeFormatter.ofPattern("MMM d @ h:mm a");
+
+    public GamePlayed(int numPlayers, int combinedScore) {
+        if (numPlayers < MIN_PLAYERS) {
+            throw new IllegalArgumentException("Number of players cannot be less than " + MIN_PLAYERS +".");
+        }
+
+        achievement = "Testing Turtles";
+
+        this.numPlayers = numPlayers;
+        this.combinedScore = combinedScore;
+        this.creationTime = LocalDateTime.now();
+    }
+
+    public int getNumPlayers() {
+        return numPlayers;
+    }
+
+    public int getCombinedScore() {
+        return combinedScore;
+    }
+
+    public String getCreationTimeString() {
+        return creationTime.format(DT_FORMAT);
+    }
+
+    public String getAchievement() {
+        return achievement;
+    }
 }
