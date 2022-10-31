12ec0c2015644243308d14fa43fa56fb3a3eade4
Kayla Lee <kel7@sfu.ca>
2022-10-31 01:04:19 -0700

    Add empty state message and dialog box
    Empty state message tells users how to add a game, and the dialog box welcomes users with more details

--- app/src/main/java/ca/cmpt276/chromiumproject/MainActivity.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/MainActivity.java
@@ -1,14 +1,44 @@
 package ca.cmpt276.chromiumproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isEmpty = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: initialize array adapter before calling checkIsEmpty()
+        if (isEmpty) {
+
+            FragmentManager manager = getSupportFragmentManager();
+            EmptyListFragment dialog = new EmptyListFragment();
+            //TODO: add string to xml
+            dialog.show(manager, "EmptyListFragment");
+
+            TextView emptyText = findViewById(R.id.emptyText);
+            emptyText.setVisibility(View.VISIBLE);
+        }
     }
+
+    private void checkIsEmpty(Adapter adapter) {
+        if (adapter.getCount() == 0) {
+            isEmpty = true;
+        }
+        else {
+            isEmpty = false;
+        }
+    }
+
 }
