61d8e880468c5e50a01f98fa172b74146d20054b
Jay <jay_an@sfu.ca>
2022-11-01 14:12:48 -0700

    Add potential achievement points feature
    Feature to be used for View Achievement UI

--- app/src/main/java/ca/cmpt276/chromiumproject/model/Achievement.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/model/Achievement.java
@@ -7,6 +7,7 @@ public class Achievement {
     private String[] achievementCollection = {"Achievement #1", "Achievement #2",
             "Achievement #3", "Achievement #4", "Achievement #5", "Achievement #6",
             "Achievement #7", "Achievement #8"};
+    private int[] potentialAchievePoints = {};
 
     //Singleton connection to GameConfig
     private GameConfig curGameConfig = GameConfig.getInstance();
@@ -59,6 +60,32 @@ public class Achievement {
         String theResult = achievementCollection[achieveNum];
 
         curAchievement = theResult;
+    }
+
+    public void setPotentialAchievePoint(int playerCount) {
+        int lowestAchieve = playerCount * curGameConfig.getPoorScore();
+        int highestAchieve = playerCount * curGameConfig.getGreatScore();
+
+        int middleGround = highestAchieve - lowestAchieve;
+
+        int partitionNum = middleGround / NUM_ACHIEVEMENTS;
+
+        //Set initial location for loop
+        int partitionMultiplier = 0;
+        int curAchieveLocation = 0;
+        int curAchieveEndBoundary = 0;
+
+        for (int i = 0; i < NUM_ACHIEVEMENTS; i++) {
+            partitionMultiplier = i * partitionNum;
+
+            //Set the Boundary for calculation purpose
+            curAchieveLocation = lowestAchieve + partitionMultiplier;
+
+            potentialAchievePoints[i] = curAchieveLocation;
+        }
+    }
+    public int[] getPotentialAchievePoint() {
+        return potentialAchievePoints;
     }
 
 }
