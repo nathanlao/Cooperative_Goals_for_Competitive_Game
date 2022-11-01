8ed46221809aea2a479e5127d2707df9d4e25705
Kayla Lee <kel7@sfu.ca>
2022-10-31 23:23:12 -0700

    Add the Record New Game Play Screen
    Added a screen to add new game records

---
+++ app/src/main/java/ca/cmpt276/chromiumproject/RecordNewGamePlay.java
@@ -0,0 +1,47 @@
+package ca.cmpt276.chromiumproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import ca.cmpt276.chromiumproject.model.GameRecord;
+
+public class RecordNewGamePlay extends AppCompatActivity {
+
+    TextView numPlayers;
+    TextView combinedScore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_new_game_play);
+//        setTitle("Record New Game Play");
+
+        numPlayers = findViewById(R.id.numPlayersInput);
+        combinedScore = findViewById(R.id.combinedScoreInput);
+
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu
+        getMenuInflater().inflate(R.menu.menu_record_new_game_played, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_save:
+                //GameRecord gameRecord = new GameRecord();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/ca/cmpt276/chromiumproject/ViewGameActivity.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/ViewGameActivity.java
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+
+import ca.cmpt276.chromiumproject.model.GameConfig;
+import ca.cmpt276.chromiumproject.model.GameRecord;
 
 public class ViewGameActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +44,23 @@ public class ViewGameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //TODO: send edit intent to record game screen
-                Intent recordGameIntent = new Intent(ViewGameActivity.this, MainActivity.class);
+                Intent recordGameIntent = new Intent(ViewGameActivity.this, RecordNewGamePlay.class);
                 startActivity(recordGameIntent);
             }
         });
     }
 
-    
+    private ArrayAdapter<String> populateGamesRecordListView() {
+        ///create list of items
+//        GameConfig gameConfig = new GameConfig();
+//        List<String> gamesPlayed = gameConfig.getGameRecordStrings();
+        //adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.games_played); //gamesPlayed);
+        ListView gamesPlayedList = (ListView) findViewById(R.id.gamesPlayedListView);
+        gamesPlayedList.setAdapter(adapter);
+        return adapter;
+    }
+
 
 
 
