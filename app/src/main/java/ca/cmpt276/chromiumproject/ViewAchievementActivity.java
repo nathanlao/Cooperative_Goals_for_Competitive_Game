2c845dd2b245b6c5e7c9b57f4b87401564dcdcf4
Jay <jay_an@sfu.ca>
2022-11-03 12:41:21 -0700

    Solidified view
    Implement other way to view achivements and score

--- app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
@@ -1,33 +1,65 @@
 package ca.cmpt276.chromiumproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ViewAchievementActivity extends AppCompatActivity {
+    String[] testListAchievements = {"One", "Two", "Three", "Four",
+            "Five", "Six", "Seven", "Eight"};
+    int[] testNumber = {1,2,3,4,5,6,7,8};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_achievement);
 
-        //getSupportActionBar().hide();
+        getSupportActionBar().hide();
 
         populateAchievements();
     }
 
     private void populateAchievements() {
-        //TODO later get actual achievement
-        String[] testListAchievements = {"One", "Two", "Three", "Four",
-                "Five", "Six", "Seven", "Eight"};
+        //TODO later get actual achievement from intent
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_view_achievement,
-                testListAchievements);
+
+        ArrayAdapter<String> adapterAchieve = new MyListAdapter();
 
         ListView achieveList = findViewById(R.id.listViewAchieveCollection);
-        achieveList.setAdapter(adapter);
+        achieveList.setAdapter(adapterAchieve);
+    }
+
+    private class MyListAdapter extends ArrayAdapter<String> {
+        public MyListAdapter() {
+            super(ViewAchievementActivity.this, R.layout.achievement_view, testListAchievements);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.achievement_view, parent, false);
+            }
+
+            String curAchieve = testListAchievements[position];
+
+            TextView achieveText = itemView.findViewById(R.id.item_achievement);
+            achieveText.setText(curAchieve);
+
+
+            int curAchScore = testNumber[position];
+            TextView scoreText = itemView.findViewById(R.id.item_achieveScore);
+            scoreText.setText("" + curAchScore);
+
+            return itemView;
+        }
     }
 }
