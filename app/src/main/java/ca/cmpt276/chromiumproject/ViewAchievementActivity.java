6a526440b9e70cfa6c75a04ddc0062231824c65c
Jay <jay_an@sfu.ca>
2022-11-03 13:10:55 -0700

    Implement data extraction
    Implement way to extract achievement data -
    achievement collections and potential score collections.
    Pass achievment's string array and int array into activity
    to show up as view in ViewAchievement activity

--- app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,13 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class ViewAchievementActivity extends AppCompatActivity {
-    String[] testListAchievements = {"One", "Two", "Three", "Four",
-            "Five", "Six", "Seven", "Eight"};
-    int[] testNumber = {1,2,3,4,5,6,7,8};
+    private static final String ACHIEVEMENT_COLLECTION_LIST =
+            "ca.cmpt276.chromiumproject - List of collections of available Achievements";
+    private static final String POTENTIAL_SCORE_LIST =
+            "ca.cmpt276.chromiumproject - List of potential achievable scores";
+
+    private String[] achievementCollections;
+    private int[] potentialScoreCollections;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +29,29 @@ public class ViewAchievementActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        extractDataFromIntent();
+
         populateAchievements();
     }
 
+    public static Intent makeIntent (Context context, String[] achieveList, int[] scoreList) {
+        Intent intent = new Intent(context, ViewAchievementActivity.class);
+
+        intent.putExtra(ACHIEVEMENT_COLLECTION_LIST, achieveList);
+        intent.putExtra(POTENTIAL_SCORE_LIST, scoreList);
+
+        return intent;
+    }
+    private void extractDataFromIntent() {
+        Intent intent = getIntent();
+        //String[] tempStrArr = {""};
+        //int[] tempIntArr = {0};
+
+        achievementCollections = intent.getStringArrayExtra(ACHIEVEMENT_COLLECTION_LIST);
+        potentialScoreCollections = intent.getIntArrayExtra(POTENTIAL_SCORE_LIST);
+    }
+
     private void populateAchievements() {
-        //TODO later get actual achievement from intent
-
-
         ArrayAdapter<String> adapterAchieve = new MyListAdapter();
 
         ListView achieveList = findViewById(R.id.listViewAchieveCollection);
@@ -38,7 +60,7 @@ public class ViewAchievementActivity extends AppCompatActivity {
 
     private class MyListAdapter extends ArrayAdapter<String> {
         public MyListAdapter() {
-            super(ViewAchievementActivity.this, R.layout.achievement_view, testListAchievements);
+            super(ViewAchievementActivity.this, R.layout.achievement_view, achievementCollections);
         }
 
         @NonNull
@@ -49,13 +71,13 @@ public class ViewAchievementActivity extends AppCompatActivity {
                 itemView = getLayoutInflater().inflate(R.layout.achievement_view, parent, false);
             }
 
-            String curAchieve = testListAchievements[position];
+            String curAchieve = achievementCollections[position];
 
             TextView achieveText = itemView.findViewById(R.id.item_achievement);
             achieveText.setText(curAchieve);
 
 
-            int curAchScore = testNumber[position];
+            int curAchScore = potentialScoreCollections[position];
             TextView scoreText = itemView.findViewById(R.id.item_achieveScore);
             scoreText.setText("" + curAchScore);
 
