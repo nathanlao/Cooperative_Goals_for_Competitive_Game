d32ae8d61056f0b33b47d598e1860d472de068db
Jay <jay_an@sfu.ca>
2022-11-02 19:12:41 -0700

    Try out different layout
    Try out different look of activity

--- app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
@@ -3,6 +3,8 @@ package ca.cmpt276.chromiumproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ViewAchievementActivity extends AppCompatActivity {
 
@@ -11,12 +13,21 @@ public class ViewAchievementActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_achievement);
 
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
 
         populateAchievements();
     }
 
     private void populateAchievements() {
+        //TODO later get actual achievement
+        String[] testListAchievements = {"One", "Two", "Three", "Four",
+                "Five", "Six", "Seven", "Eight"};
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.activity_view_achievement,
+                testListAchievements);
+
+        ListView achieveList = findViewById(R.id.listViewAchieveCollection);
+        achieveList.setAdapter(adapter);
     }
 }
