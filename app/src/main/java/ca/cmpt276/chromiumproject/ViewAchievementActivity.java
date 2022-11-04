571433a67dce3cd1db06a4549f75bc9fb4db35bf
Jay <jay_an@sfu.ca>
2022-11-03 21:23:18 -0700

    Add Special Achievement at Working test phase
    Add special when achieved lower than lowest score.
    Push before getting actual data from intent

--- app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/ViewAchievementActivity.java
@@ -13,6 +13,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * ViewAchievementActivity gets list of possible achievements you can achieve,
  * as well as potential achievable point list passed down from intent,
@@ -23,9 +28,15 @@ public class ViewAchievementActivity extends AppCompatActivity {
             "ca.cmpt276.chromiumproject - List of collections of available Achievements";
     private static final String POTENTIAL_SCORE_LIST =
             "ca.cmpt276.chromiumproject - List of potential achievable scores";
+    private static final String SPECIAL_ACHIEVEMENT =
+            "Toddler's Baby Step";
 
-    private String[] achievementCollections;
-    private int[] potentialScoreCollections;
+    private String[] achievementCollections = {"One", "Two", "Three", "Four",
+            "Five", "Six", "Seven", "Eight"};
+    private List<String> actualAchievementList = new ArrayList<>();
+
+    private int[] potentialScoreCollections = {1,2,3,4,5,6,7,8};
+    private List<Integer> actualScoreList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +45,18 @@ public class ViewAchievementActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        extractDataFromIntent();
+        //extractDataFromIntent();
+        includeSpecialAchievement();
 
         populateAchievements();
+    }
+
+    private void includeSpecialAchievement() {
+        actualAchievementList = new ArrayList<String>(Arrays.asList(achievementCollections));
+        actualAchievementList.add(0, SPECIAL_ACHIEVEMENT);
+        actualScoreList =
+                new ArrayList<Integer>(Arrays.stream(potentialScoreCollections).boxed().collect(Collectors.toList()));
+        actualScoreList.add(0, potentialScoreCollections[0]);
     }
 
     public static Intent makeIntent (Context context, String[] achieveList, int[] scoreList) {
@@ -63,7 +83,7 @@ public class ViewAchievementActivity extends AppCompatActivity {
 
     private class MyListAdapter extends ArrayAdapter<String> {
         public MyListAdapter() {
-            super(ViewAchievementActivity.this, R.layout.achievement_view, achievementCollections);
+            super(ViewAchievementActivity.this, R.layout.achievement_view, actualAchievementList);
         }
 
         @NonNull
@@ -74,15 +94,26 @@ public class ViewAchievementActivity extends AppCompatActivity {
                 itemView = getLayoutInflater().inflate(R.layout.achievement_view, parent, false);
             }
 
-            String curAchieve = achievementCollections[position];
+            String curAchieve = actualAchievementList.get(position);
 
             TextView achieveText = itemView.findViewById(R.id.item_achievement);
             achieveText.setText(curAchieve);
+            TextView reqText = itemView.findViewById(R.id.item_req);
+            reqText.setText("Required Score: ");
 
+            if (position == 0) {
+                int curAchScore = actualScoreList.get(position);
+                TextView scoreText = itemView.findViewById(R.id.item_achieveScore);
+                scoreText.setText("" + "<" + curAchScore);
 
-            int curAchScore = potentialScoreCollections[position];
-            TextView scoreText = itemView.findViewById(R.id.item_achieveScore);
-            scoreText.setText("" + curAchScore);
+                reqText.setText("Earned by Score: ");
+                achieveText.setText("Special Achievement: " + curAchieve);
+            }
+            if (position != 0) {
+                int curAchScore = actualScoreList.get(position);
+                TextView scoreText = itemView.findViewById(R.id.item_achieveScore);
+                scoreText.setText("" + curAchScore);
+            }
 
             return itemView;
         }
