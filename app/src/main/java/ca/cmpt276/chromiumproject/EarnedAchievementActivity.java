76a3c30882ac6390450d91f1c023c34b70fcf676
Kayla Lee <kel7@sfu.ca>
2022-11-14 22:56:49 -0800

    Added image, animation, and sound effect to EarnedAchievementActivity
    The images are animated to move up the page while a cheering sound effect plays.

--- app/src/main/java/ca/cmpt276/chromiumproject/EarnedAchievementActivity.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/EarnedAchievementActivity.java
@@ -2,23 +2,43 @@ package ca.cmpt276.chromiumproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 
-/**Background photo from https://unsplash.com/photos/xplrF8WMitE, firework animation from https://www.pngegg.com/en/search?q=fireworks+clipart
- *
+/**EarnedAchievementActivity is displayed after a user saves a new game record and earns an achievement level. A congratulatory message appears alongside a
+ * sound effect, image, and animation.
+ * Background photo from https://unsplash.com/photos/xplrF8WMitE, firework animation from cupids@aphrodite.global
+ * Sound from https://pixabay.com/sound-effects/pleased-crowdflac-14484/
  */
 public class EarnedAchievementActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME = 6*1000;
+    public static final int ANIMATION_DURATION = 4000;
+    public static final int FIREWORKS1_X_TRANLSATION = 500;
+    public static final int FIREWORKS1_Y_TRANSLATION = -500;
+    public static final int FIREWORKS2_X_TRANLSATION = -500;
+    public static final int FIREWORKS2_Y_TRANSLATION = -500;
+    private static int SPLASH_TIME = 10*1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earned_achievement);
         setTitle("Congratulations!");
+
+        ImageView fireworks1 = findViewById(R.id.fireworksAnimation);
+        ImageView fireworks2 = findViewById(R.id.fireworksAnimation2);
+
+        runAnimation(fireworks1, FIREWORKS1_X_TRANLSATION, FIREWORKS1_Y_TRANSLATION);
+        runAnimation(fireworks2, FIREWORKS2_X_TRANLSATION, FIREWORKS2_Y_TRANSLATION);
+        MediaPlayer cheeringAudio = MediaPlayer.create(EarnedAchievementActivity.this, R.raw.cheering_audio);
+        cheeringAudio.start();
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -30,8 +50,42 @@ public class EarnedAchievementActivity extends AppCompatActivity {
         skipBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                cheeringAudio.stop();
                 finish();
             }
         });
+    }
+
+    private void runAnimation(ImageView fireworks, int xTranlsation, int yTranslation) {
+        //animation tutorial: https://stackoverflow.com/questions/12983681/translateanimation-on-imageview-android
+
+        int xPosition = fireworks.getLeft();
+        int yPosition = fireworks.getTop();
+
+        AnimationSet animationSet = new AnimationSet(false);
+
+        Animation animation = new TranslateAnimation(xPosition, xPosition+ xTranlsation, yPosition, yPosition+ yTranslation);
+        animation.setDuration(ANIMATION_DURATION);
+        animation.setFillAfter(true);
+        animation.setFillEnabled(true);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        animationSet.addAnimation(animation);
+        fireworks.startAnimation(animationSet);
     }
 }
