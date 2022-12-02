f1c5a3eda964fd876537ddccd7e5325240bda06f
cza95 <chelsea_zhao@sfu.ca>
2022-12-01 21:01:58 -0800

    Add class comments, organize methods, extract constants
    Clean up class formatting, improve code quality

--- app/src/main/java/ca/cmpt276/chromiumproject/PhotoHelper.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/PhotoHelper.java
@@ -25,14 +25,21 @@ import java.util.Date;
 
 import ca.cmpt276.chromiumproject.model.PhotoContainer;
 
+/**
+ * PhotoHelper is a utility class that provides static methods for saving and loading Bitmap photos between UI and model
+ * Bitmap is stored/loaded as Uri or File depending on Android version
+ * Saves photos to Android camera roll
+ */
+
 public final class PhotoHelper {
     private static final String TAG = "Catch PhotoManager Exception";
+    public static final String MIME_TYPE = "image/jpeg";
+    public static final int BMP_COMPRESS_QUALITY = 90;
 
     private PhotoHelper() {
-        // private constructor
+        // private constructor to prevent instantiation
     }
 
-    // reference: https://stackoverflow.com/questions/56904485/how-to-save-an-image-in-android-q-using-mediastore
     public static void savePhotoAndStoreInModel(Context context, PhotoContainer photoContainer, Bitmap bmp) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             Uri uri = savePhotoToMediaStore(context, bmp);
@@ -40,8 +47,8 @@ public final class PhotoHelper {
                 photoContainer.setPhotoDataString(uri.toString());
             }
         } else {
-            File photoFile = getOutputMediaFile(context);
-            savePhotoToExternalStorage(context, photoFile, bmp);
+            File photoFile = getOutputPhotoFile();
+            savePhotoToExternalStorage(photoFile, bmp);
             photoContainer.setPhotoDataString(photoFile.getAbsolutePath());
         }
     }
@@ -65,50 +72,26 @@ public final class PhotoHelper {
         return bitmap;
     }
 
-    // reference: https://stackoverflow.com/questions/15662258/how-to-save-a-bitmap-on-internal-storage
-    // Used to store photo to storage for BELOW API ver 29 (Q)
-    public static void savePhotoToExternalStorage(Context context, File photoFile, Bitmap bmp) {
-        if (photoFile == null) {
-            throw new NullPointerException("Error creating media file, check storage permissions: ");
-        }
-        try {
-            FileOutputStream imageOutStream = new FileOutputStream(photoFile);
-            bmp.compress(Bitmap.CompressFormat.JPEG, 90, imageOutStream);
-            imageOutStream.close();
-        } catch (FileNotFoundException e) {
-            Log.d(TAG, "File not found: " + e.getMessage());
-        } catch (IOException e) {
-            Log.d(TAG, "Error accessing file: " + e.getMessage());
-        }
-    }
-
-    // reference: https://stackoverflow.com/questions/15662258/how-to-save-a-bitmap-on-internal-storage
-    /** Create a File for saving an image or video */
-    public static File getOutputMediaFile(Context context){
-        String photoPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
-        String fileName = createMediaFileName();
-        File photoFile = new File(photoPath, fileName);
-
-        return photoFile;
-    }
-
-    // reference: https://medium.com/@vishrut.goyani9/scoped-storage-in-android-writing-deleting-media-files-ee6235d30117
+    /**
+     * Store photo to MediaStore, bypassing need for permissions. Requires at least API 29 (Q)
+     * refs used:
+     * https://medium.com/@vishrut.goyani9/scoped-storage-in-android-writing-deleting-media-files-ee6235d3011
+     * https://stackoverflow.com/questions/56904485/how-to-save-an-image-in-android-q-using-mediastore
+     */
     @RequiresApi(api = Build.VERSION_CODES.Q)
-    public static Uri savePhotoToMediaStore(Context context, Bitmap bitmap) {
-        OutputStream imageOutStream;
+    private static Uri savePhotoToMediaStore(Context context, Bitmap bitmap) {
         ContentValues contentValues = new ContentValues();
 
-        String mediaFileName = createMediaFileName();
-
+        String mediaFileName = createPhotoFileName();
         contentValues.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, mediaFileName);
-        contentValues.put(MediaStore.Images.ImageColumns.MIME_TYPE, "image/jpeg");
-        contentValues.put(MediaStore.Images.ImageColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM); // TODO: can also use DIRECTORY_PICTURES?
+        contentValues.put(MediaStore.Images.ImageColumns.MIME_TYPE, MIME_TYPE);
+        contentValues.put(MediaStore.Images.ImageColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM);
 
         ContentResolver contentResolver = context.getContentResolver();
         Uri insert = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
         if (insert != null) {
             try {
-                imageOutStream = contentResolver.openOutputStream(insert);
+                OutputStream imageOutStream = contentResolver.openOutputStream(insert);
                 if (imageOutStream == null) {
                     throw new IOException("Failed to open output stream.");
                 } else if (bitmap.compress(Bitmap.CompressFormat.JPEG, 90, imageOutStream)) {
@@ -127,9 +110,26 @@ public final class PhotoHelper {
         return null;
     }
 
+    // Used to store photo to storage for Android versions BELOW API 29 (Q)
+    // ref: https://stackoverflow.com/questions/15662258/how-to-save-a-bitmap-on-internal-storage
+    private static void savePhotoToExternalStorage(File photoFile, Bitmap bmp) {
+        if (photoFile == null) {
+            throw new NullPointerException("Error creating media file, check storage permissions: ");
+        }
+        try {
+            FileOutputStream imageOutStream = new FileOutputStream(photoFile);
+            bmp.compress(Bitmap.CompressFormat.JPEG, BMP_COMPRESS_QUALITY, imageOutStream);
+            imageOutStream.close();
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "Error accessing file: " + e.getMessage());
+        }
+    }
+
     // ref: https://stackoverflow.com/questions/65210522/how-to-get-bitmap-from-imageuri-in-api-level-30
     @RequiresApi(api = Build.VERSION_CODES.P)
-    public static Bitmap getBitmapFromUri(Context context, Uri uri) {
+    private static Bitmap getBitmapFromUri(Context context, Uri uri) {
         ContentResolver contentResolver = context.getContentResolver();
         Bitmap bitmap = null;
         try {
@@ -140,15 +140,9 @@ public final class PhotoHelper {
         }
         return bitmap;
     }
-    
-    private static String createMediaFileName() {
-        String timeStamp = SimpleDateFormat.getDateTimeInstance().format(new Date());
-        String fileName ="IMG_"+ timeStamp +".jpg";
-        return fileName;
-    }
 
-    // reference: https://stackoverflow.com/questions/17674634/saving-and-reading-bitmaps-images-from-internal-memory-in-android
-    public static Bitmap getBitmapFromFilePath(String filePath) {
+    // ref: https://stackoverflow.com/questions/17674634/saving-and-reading-bitmaps-images-from-internal-memory-in-android
+    private static Bitmap getBitmapFromFilePath(String filePath) {
         Bitmap bitmap = null;
         try {
             File photoFile = new File(filePath);
@@ -162,5 +156,17 @@ public final class PhotoHelper {
             e.printStackTrace();
         }
         return bitmap;
+    }
+
+    private static File getOutputPhotoFile(){
+        String photoPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).toString();
+        String fileName = createPhotoFileName();
+
+        return new File(photoPath, fileName);
+    }
+
+    private static String createPhotoFileName() {
+        String timeStamp = SimpleDateFormat.getDateTimeInstance().format(new Date());
+        return "IMG_"+ timeStamp +".jpg";
     }
 }
