ba6000aa285914afcfdc906c15dc7f911a35d59f
cza95 <chelsea_zhao@sfu.ca>
2022-12-01 18:40:51 -0800

    Change PhotoContainer to store String reference to photo
    PhotoContainer stores a String that represents the photo's data, whether as a Uri or File path. This makes PhotoContainer more flexible to different types of photo implementations.

--- app/src/main/java/ca/cmpt276/chromiumproject/model/PhotoContainer.java
+++ app/src/main/java/ca/cmpt276/chromiumproject/model/PhotoContainer.java
@@ -1,28 +1,27 @@
 package ca.cmpt276.chromiumproject.model;
 
-import android.graphics.Bitmap;
+import java.io.File;
 
-/** PhotoContainer is an abstract class used to create objects which contain a Bitmap reference to a photo.
- *  Please use deletePhoto() to free up memory when deleting a concrete PhotoContainer object
+/** PhotoContainer is an abstract class used to create objects which contains a reference to a single photo
+ *  The photo is stored as a data String. Supports Uri (uri.toString()) and File (file path)
  */
 
 public abstract class PhotoContainer {
-    private Bitmap photo;
+    private String photoDataString;
 
-    public boolean hasValidPhoto() {
-        return photo != null;
+    public boolean hasPhotoFile() {
+        if (photoDataString == null) {
+            return false;
+        }
+        File photo = new File(photoDataString);
+        return photo.isFile();
     }
 
-    public Bitmap getPhoto() {
-        return photo;
+    public String getPhotoDataString() {
+        return photoDataString;
     }
 
-    public void setPhoto(Bitmap photo) {
-        this.photo = photo;
-    }
-
-    public void deletePhoto() {
-        photo.recycle();
-        this.photo = null;
+    public void setPhotoDataString(String photoDataString) {
+        this.photoDataString = photoDataString;
     }
 }
